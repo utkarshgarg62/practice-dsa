323231584d22c9aa8c34e5ec2f52448cc16ce26e
utkarshgarg62 <utkarshgarg143@gmail.com>
2022-09-14 19:54:39 +0530

    dsa

--- sorting/merge_sort.js
+++ sorting/merge_sort.js
@@ -1,20 +1,57 @@
 // arr = [233, 12, 6, 3, 89, 34, 56]
 
-function merge_sort(arr) {
+function merge_sort(arr, start, end) {
 
     if (start >= end) return
-    let mid = Math.round((start + end) / 2)
+    let mid = Math.floor((start + end) / 2)
     merge_sort(arr, start, mid)
     merge_sort(arr, mid + 1, end)
 
     merge(arr, start, mid, end)
-
+    
+    // arr1: [start...mid]
+    // arr2: [mid+1...end]
     function merge(arr, start, mid, end) {
+        let arr1 = []
+        let arr2 = []
 
+        let lenArr1 = mid - start + 1
+        let lenArr2 = end - mid
+        for (let i = 0; i < lenArr1; i++) {
+            arr1[i] = arr[start + i]
+        }
+        for (let i = 0; i < lenArr2; i++) {
+            arr2[i] = arr[mid + 1 + i]
+        }
+        let a = 0 //index of arr1
+        let b = 0 //index of arr2
+        let c = start //index of arr
+
+        while (a < lenArr1 && b < lenArr2) {
+            if (arr1[a] <= arr2[b]) {
+                arr[c] = arr1[b]
+                a++
+            }
+            else {
+                arr[c] = arr2[b]
+                b++
+            }
+        }
+
+        while (a < lenArr1) {
+            arr[c] = arr1[a]
+            a++
+            c++
+        }
+        while (b < lenArr2) {
+            arr[c] = arr2[b]
+            b++
+            c++
+        }
     }
 }
 
 
 
-let res = merge_sort([233, 12, 6, 3, 89, 34, 56])
+let res = merge_sort([233, 12, 6, 3, 89, 34, 56], 0, 7)
 console.log(res)
