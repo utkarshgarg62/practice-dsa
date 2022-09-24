861d0295a703ce3040983212cce503b5a27e10b8
utkarshgarg62 <utkarshgarg143@gmail.com>
2022-09-24 12:55:24 +0530

    dsa

---
+++ questions/dequeue/dequeue.js
@@ -0,0 +1,14 @@
+// # Firstly u have to install dequeue => npm i dequeue
+// # Then import dequeue => let Dequeue = require("dequeue")
+
+let dq = new Dequeue()  //To Define Empty Dq
+
+dq.length  //To find length
+dq.push(value)  //To add value from last
+dq.pop()    //To remove value from last
+dq.unshift(value)   //To add value from first
+dq.shift()  //To remove value from first
+dq.first()  //To select element from first
+dq.last()   //To select element from last
+dq.empty()  //To empty the dq
+
