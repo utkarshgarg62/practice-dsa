dee11aa3a06e798f71096e2576a30525cd3dcfc6
utkarshgarg62 <utkarshgarg143@gmail.com>
2022-10-15 13:54:48 +0530

    dsa

--- questions/Data Structure/graph/1. Introduction.js
+++ questions/Data Structure/graph/1. Introduction.js
@@ -24,3 +24,67 @@
 // Representation of the graph-
 // 1. Adjacency Matrix.
 // 2. Adjacency List
+
+// Types of graph-
+// 1. Directed graph.
+// 2. Undirected graph.
+
+// UNIDIRECTED GRAPH -----------------
+// Undirected graphs have edges that do not have a direction. The edges 
+// indicate a two-way relationship, in that each edge can be traversed 
+// in both directions. This figure shows a simple undirected graph with 
+// five nodes and five edges.
+
+//         A ----------------- B
+//         |                    \
+//         |                     \
+//         |                      E
+//         |                     /
+//         |                    /
+//         C ----------------- D
+
+
+// DIRECTED GRAPH ------------------
+// Directed graphs have edges with direction. The edges indicate a one-way 
+// relationship, in that each edge can only be traversed in a single direction. 
+// This figure shows a simple directed graph with five nodes and five edges.
+
+                    
+//         A --------->------- B
+//         |                    \
+//         |                     \
+//         |                      E
+//         |                     /
+//         |                    /
+//         C --------->------- D
+
+
+// WEIGHTED GRAPH -----------------
+// A weighted graph is a graph in which each branch is given a numerical weight.
+//  A weighted graph is therefore a special type of labeled graph in which the 
+//  labels are numbers (which are usually taken to be positive).
+
+//                  10
+//         A ----------------- B
+//         |                    \ 5
+//         |                     \
+//      16 |                      E
+//         |                     /  
+//         |                    / 8
+//         C ----------------- D
+//                  11
+
+
+// RealLife Example -
+// 1.Google Map
+// 2.Routing (newtorking)
+// 3.Facebook (friends suggestion)
+// 4.Airline -Routes
+
+
+// Degree Of vertex or vertices-
+//      -------A------
+//            / \
+//           /   \
+// So, degree of A vertix is 4.
+// Degree of vertex = total no of incident edge.
--- questions/Data Structure/graph/2. imple. of Graph.js
+++ questions/Data Structure/graph/2. imple. of Graph.js
@@ -48,3 +48,47 @@ graph.addEdge("C", "E")
 graph.addEdge("C", "D")
 
 graph.printGraph() 
+
+console.log("**************")
+
+
+class Graph1{
+    constructor(no_of_vertices){
+        this.no_of_vertices= no_of_vertices
+        this.adjList = new Map()
+    }
+    addVertex(V){
+        this.adjList.set(V,[])
+    }
+    addEdge(V,target){
+        this.adjList.get(V).push(target)
+        this.adjList.get(target).push(V)
+    }
+    printGraph(){
+        let keys = this.adjList.keys()
+        for(let key of keys){
+            console.log(key,"->",this.adjList.get(key))
+        }
+    }
+}
+
+let g = new Graph1(6)
+g.addVertex("A")
+g.addVertex("B")
+g.addVertex("C")
+g.addVertex("D")
+g.addVertex("E")
+g.addVertex("F")
+
+g.addEdge("A","B")
+g.addEdge("A","C")
+g.addEdge("A","D")
+g.addEdge("B","E")
+g.addEdge("C","D")
+g.addEdge("F","A")
+g.addEdge("F","B")
+g.addEdge("F","C")
+g.addEdge("F","E")
+
+console.log(g)
+g.printGraph()
